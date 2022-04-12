a563de992bc003c7baf99ed4ecf6bc51eb58573d
Michelle D <45058019+TeaFourAll@users.noreply.github.com>
2022-04-12 15:09:50 -0400

    sleep deprived

---
+++ model/User.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.io.*;
+import java.util.*;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -7351729135012380019L;
+
+	String username;
+
+	List<Album> userAlbums;
+	
+
+	public User(String user) {
+		this.username=user;
+		this.userAlbums=new ArrayList<>();
+	}
+
+	public String toString() {
+		return this.getUsername();
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public List<Album> getAlbums(){
+		return this.userAlbums;
+	}
+
+}
